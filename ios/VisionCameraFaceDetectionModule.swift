44f4922a9163ef4d22bfce37322562958106f4db
Yudi Alviska <yudi.a@weefer.co.id>
2025-06-25 11:33:25 +0700

    chore: test call swift from objC

---
+++ ios/VisionCameraFaceDetectionModule.swift
@@ -0,0 +1,18 @@
+//
+//  VisionCameraFaceDetectionModule.swift
+//  VisionCameraFaceDetection
+//
+//  Created by Yudi Edri Alviska on 25/06/25.
+//
+
+import Foundation
+
+@objcMembers class VisionCameraFaceDetectionModule: NSObject {
+  func getHello() -> String {
+    return "Hello"
+  }
+  
+  func getMultiply(a: Double, b: Double) -> NSNumber {
+    return NSNumber(value: a * b)
+  }
+}
