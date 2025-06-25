60b80bf5190700cd67cf3d5112b5b8a05b625969
Yudi Alviska <yudi.a@weefer.co.id>
2025-06-25 09:22:33 +0700

    chore: test install package

--- src/NativeVisionCameraFaceDetection.ts
+++ src/NativeVisionCameraFaceDetection.ts
@@ -5,4 +5,6 @@ export interface Spec extends TurboModule {
   multiply(a: number, b: number): number;
 }
 
-export default TurboModuleRegistry.getEnforcing<Spec>('VisionCameraFaceDetection');
+export default TurboModuleRegistry.getEnforcing<Spec>(
+  'VisionCameraFaceDetection'
+);
